98cd8028fba779c496d0ae2384b1cd1de1b443fd
msiwak <msiwak@edipresse.pl>
2017-07-22 10:41:42 +0200

    prop 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    public productsList = [];
+
+    constructor() {
+        for (let i = 0; i < 15; i++) {
+            this.productsList.push(new Product('Product number ' + i));
+        }
+    }
+}
+
+interface ProductInteface {
+    name: string;
+}
+
+class Product implements ProductInteface {
+    public name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
 }
