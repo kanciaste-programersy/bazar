8bd7d4c29ef417a029b98f10182e163a34363bde
Bambrowicz <milbam@duni.org>
2017-07-22 10:55:35 +0200

    nowe prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,9 @@ export class AppComponent {
 
 public products: Array<Object> = [
         {name: 'fidget spinner', price: 10.00, description:'popularne w gimnazjum', promoted:true },
-        {name: 'piłka', price: 20.00, description:'Euro 2012', promoted:false}
+        {name: 'piłka', price: 20.00, description:'Euro 2012', promoted:false},
+        {name: 'pizza', price: 27.00, description:'Funghi na podójnymcieście. Dodatkowy ser i sos czosnkowy dla stunetów GRATIS!', promoted:true},
+        {name: 'skarpetki', price: 6.00, description:'Czarno-niebieskie skarpetki do garnituru', promoted:false}
     ];
 
   title = 'app';
