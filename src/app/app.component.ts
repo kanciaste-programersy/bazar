c8b4b76eba59da4f7c2342b4b0463400fe971197
Bambrowicz <milbam@duni.org>
2017-07-22 10:29:41 +0200

    bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+public products: Array<Object> = [
+        {nazwa: 'fidget spinner', cena: 10.00, opis:'popularne w gimnazjum' },
+        {}
+    ];
+
   title = 'app';
 }
