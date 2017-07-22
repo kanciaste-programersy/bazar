debd29f962cb05d247b32994fd0b51d7583782e0
Bambrowicz <milbam@duni.org>
2017-07-22 11:10:26 +0200

    all products component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { AllProductsComponent } from './all-products/all-products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    AllProductsComponent
   ],
   imports: [
     BrowserModule
