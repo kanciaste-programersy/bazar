0f05472245396bcb358615e92d76ee0237cbc3a8
LukeAheadNET <dev@lukeahead.net>
2017-07-22 11:41:23 +0200

    product images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
 public products: Array<Object> = [
-        {name: 'fidget spinner', price: 10.00, description:'popularne w gimnazjum', promoted:true },
-        {name: 'piłka', price: 20.00, description:'Euro 2012', promoted:false},
-        {name: 'pizza', price: 27.00, description:'Funghi na podójnym cieście. Dodatkowy ser i sos czosnkowy dla studentów GRATIS!', promoted:true},
-        {name: 'skarpetki', price: 6.00, description:'Czarno-niebieskie skarpetki do garnituru', promoted:false}
+        {name: 'fidget spinner', price: 10.00, description:'popularne w gimnazjum', promoted:true, image: 'default.png' },
+        {name: 'piłka', price: 20.00, description:'Euro 2012', promoted:false, image: 'ball.jpg' },
+        {name: 'pizza', price: 27.00, description:'Funghi na podójnym cieście. Dodatkowy ser i sos czosnkowy dla studentów GRATIS!', promoted: true, image: 'default.png' },
+        {name: 'skarpetki', price: 6.00, description:'Czarno-niebieskie skarpetki do garnituru', promoted: false, image: 'socks.gif' }
     ];
 
   title = 'app';
